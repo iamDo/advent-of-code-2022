82ed42eb75c813d5c4c0388d8f3adee57bbe9f4e
ppolmans <pieter.polmans@delta.blue>
2023-12-02 13:40:23 +0100

    day 3.1
    Signed-off-by: ppolmans <pieter.polmans@delta.blue>

--- day-3.1/main.py
+++ day-3.1/main.py
@@ -2,6 +2,10 @@
 
 INPUT_FILE = 'input.txt'
 
+ALPHABET_LOWERCASE = [chr(i) for i in range(97, 123)]
+ALPHABET_UPPERCASE = [chr(i) for i in range(65, 91)]
+ALPHABET = ALPHABET_LOWERCASE + ALPHABET_UPPERCASE
+
 
 def get_input():
     lines = []
@@ -9,3 +13,27 @@ def get_input():
         lines = f.readlines()
 
     return lines
+
+
+def get_item_priority(item):
+    return ALPHABET.index(item) + 1
+
+
+def line_to_ruckack(line):
+    compartment_size = int(len(line)/2)
+    return [line[:compartment_size], line[compartment_size:]]
+
+
+def find_duplicate(rucksack):
+    return (set(rucksack[0]) & set(rucksack[1])).pop()
+
+
+def main():
+    lines = get_input()
+    rucksacks = [line_to_ruckack(line) for line in lines]
+    duplicates = [find_duplicate(rucksack) for rucksack in rucksacks]
+    print(sum((get_item_priority(item) for item in duplicates)))
+
+
+if __name__ == '__main__':
+    main()
